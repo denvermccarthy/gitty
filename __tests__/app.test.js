db4d575c926ff6797cc40393bd12b9b8cf9de887
Denver McCarthy <dmichaelspdx@gmail.com>
2022-06-26 19:15:30 -0700

    fixed issue with tests, added mock functions

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+jest.mock('../lib/services/github');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
@@ -11,19 +12,19 @@ describe('backend-express-template routes', () => {
     const res = await request(app).get('/api/v1/github/login');
 
     expect(res.header.location).toMatch(
-      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7891\/api\/v1\/github\/callback/i
     );
   });
   test('callback route should return user data', async () => {
     const res = await request
       .agent(app)
-      .get('/api/v1/github/callback?')
+      .get('/api/v1/github/callback?code=900')
       .redirects(1);
 
     expect(res.body).toEqual({
-      id: expect.any(String),
-      username: 'fake_github_user',
-      email: 'not-real@example.com',
+      id: expect.any(Number),
+      username: 'mocked_user',
+      email: 'test@testing.com',
       avatar: expect.any(String),
       iat: expect.any(Number),
       exp: expect.any(Number),
