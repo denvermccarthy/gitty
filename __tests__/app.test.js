df197f12b151653af75e04e095762c9a9e7b9158
Denver McCarthy <dmichaelspdx@gmail.com>
2022-06-26 18:16:38 -0700

    profile data loaded, added testing before I move on to modeling

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,12 +7,27 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('login page should redirect to github', async () => {
+  test('login page should redirect to github', async () => {
     const res = await request(app).get('/api/v1/github/login');
 
     expect(res.header.location).toMatch(
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
     );
+  });
+  test('callback route should return user data', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/callback?')
+      .redirects(1);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'fake_github_user',
+      email: 'not-real@example.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
   });
   afterAll(() => {
     pool.end();
